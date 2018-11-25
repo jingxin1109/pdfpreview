edbbdde15bb018591ca19544007d4142daa8d3d3
jingxin1109 <1935232527@qq.com>
2018-11-26 01:51:54 +0800

    转换文档

---
+++ src/com/jingxin/MyServlet.java
@@ -0,0 +1,50 @@
+package com.jingxin;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/myServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MyServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		File file = new File("G:\\PDF-JS\\file\\pdf\\社会实践调查报告.pdf");
+		String id = request.getParameter("id");
+		System.out.println(id);
+		BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+		byte[] data = new byte[inputStream.available()];
+		inputStream.read(data);
+		response.getOutputStream().write(data);
+		inputStream.close();
+ 	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
